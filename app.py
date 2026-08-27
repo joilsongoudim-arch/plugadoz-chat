e0f1ab7c2d6ff1975b076edb3ceb57d38ba98c05
joilsongoudim-arch <joilsongoudim@gmail.com>
2026-08-26 23:14:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -64,3 +64,4 @@ def handle_message(data):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
+    
