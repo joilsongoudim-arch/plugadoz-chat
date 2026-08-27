a7956d96ab44140ba6cfa16645feedfd635c9baa
joilsongoudim-arch <joilsongoudim@gmail.com>
2026-08-27 01:33:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_socketio import SocketIO, emit, join_room, leave_room
+from flask_socketio import SocketIO, emit
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'chave_secreta_super_segura'
 socketio = SocketIO(app)
 
-# Dicionário temporário para guardar os usuários cadastrados { "usuario": "senha" }
 usuarios_cadastrados = {}
-
-# Lista de usuários atualmente online no chat
-usuarios_online = {}
 
 @app.route('/', methods=['GET'])
 def index():
@@ -51,22 +47,6 @@ def register():
 def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
-
-@socketio.on('connect')
-def handle_connect():
-    # Se o usuário estiver na sessão, adiciona à lista de online
-    if 'username' in session:
-        username = session['username']
-        usuarios_online[request.sid] = username
-        # Envia a lista atualizada para todo mundo no chat
-        emit('atualizar_usuarios', list(usuarios_online.values()), broadcast=True)
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    if request.sid in usuarios_online:
-        usuarios_online.pop(request.sid)
-        # Atualiza a lista para todo mundo quando alguém sai
-        emit('atualizar_usuarios', list(usuarios_online.values()), broadcast=True)
 
 @socketio.on('enviar_mensagem')
 def handle_message(data):
